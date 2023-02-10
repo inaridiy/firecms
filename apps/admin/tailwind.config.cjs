3cbaf9a4d1271cde019c6ef03884693c3da6cb11
inaridiy <inainainariinainari@gmail.com>
2023-02-10 16:05:07 +0900

    feat: theme

--- apps/admin/tailwind.config.cjs
+++ apps/admin/tailwind.config.cjs
@@ -3,7 +3,17 @@ module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				content: 'var(--color-text)',
+				neutral: 'var(--color-neutral)',
+				'neutral-content': 'var(--color-neutral-content)',
+				primary: 'var(--color-primary)',
+				'primary-content': 'var(--color-primary-content)',
+				secondary: 'var(--color-secondary)',
+				'secondary-contetn': 'var(--color-secondary-content)'
+			}
+		}
 	},
 	plugins: []
 };
