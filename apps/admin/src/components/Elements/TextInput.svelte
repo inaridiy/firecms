771eac159e155484c2dbde40398f9b70944bca22
inaridiy <inainainariinainari@gmail.com>
2023-02-24 14:21:31 -0700

    feat: Login Page

---
+++ apps/admin/src/components/Elements/TextInput.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import clsx from 'clsx';
+
+	export let id: string;
+	export let label = '';
+	export let value: string | number = '';
+	let addContainerClass = '';
+	let addInputClass = '';
+	export { addInputClass as inputClass, addContainerClass as containerClass };
+
+	let containerClass: string;
+	$: containerClass = clsx('flex flex-col gap-1', addContainerClass);
+
+	let inputClass: string;
+	$: inputClass = clsx(
+		'rounded-box border-base-content w-full border-2 p-2 shadow-sm',
+		addInputClass
+	);
+</script>
+
+<div class={containerClass}>
+	<label for={id} class="block font-medium text-base-content">{label}</label>
+	<input
+		{...$$restProps}
+		type="text"
+		bind:value
+		on:blur
+		on:change
+		on:click
+		on:contextmenu
+		on:focus
+		on:keydown
+		on:keypress
+		on:keyup
+		on:mouseover
+		on:mouseenter
+		on:mouseleave
+		on:paste
+		on:input
+		class={inputClass}
+	/>
+</div>
